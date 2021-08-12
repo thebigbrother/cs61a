513e10a3e85e6b9bbf1e3fdd775e80bb99217c07
thebigbrother <jialu.xie@gmail.com>
2021-08-12 16:21:27 -0700

    Initial commit

---
+++ homeworks/hw01/test if function.py
@@ -0,0 +1,58 @@
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 3+2, 3-2)
+    1
+    >>> if_function(3>2, 3+2, 3-2)
+    5
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+
+def with_if_statement():
+    """
+    >>> result = with_if_statement()
+    2
+    >>> print(result)
+    None
+    """
+    if c():
+        return t()
+    else:
+        return f()
+
+
+def with_if_function():
+    """
+    >>> result = with_if_function()
+    1
+    2
+    >>> print(result)
+    None
+    """
+    return if_function(c(), t(), f())
+
+
+def c():
+    """*** YOUR CODE HERE ***"""
+    return None
+
+
+def t():
+    """*** YOUR CODE HERE ***"""
+    print(1)
+
+
+def f():
+    """*** YOUR CODE HERE ***"""
+    print(2)
+
+
